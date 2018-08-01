a23f2ba66ddb22637237f92bdddf3179db70a436
lokmeinmatz <matze.kind@web.de>
2018-08-01 16:17:05 +0200

    Working new Messages card

--- src/components/ChatPreview.vue
+++ src/components/ChatPreview.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-list-tile avatar>
-    <v-list-tile-avatar>
-      <img :src="item.avatar">
-    </v-list-tile-avatar>
-
+  <v-list-tile avatar @click="console.log('')">
+    <v-badge left overlap>
+      <span slot="badge">{{message.unreadMessageCount}}</span>
+      <v-list-tile-avatar>
+        <img :src="message.avatar">
+      </v-list-tile-avatar>
+    </v-badge>
     <v-list-tile-content>
-      <v-list-tile-title>{{item.message}}</v-list-tile-title>
-      <v-list-tile-sub-title v-html="item.meta"></v-list-tile-sub-title>
+      <v-list-tile-title>{{message.content}}</v-list-tile-title>
+      <v-list-tile-sub-title v-html="message.meta"></v-list-tile-sub-title>
     </v-list-tile-content>
   </v-list-tile>
 </template>
@@ -18,7 +20,7 @@
 export default {
   name: 'ChatPreview',
   props: {
-    item: Object
+    message: Object
   }
 }
 </script>
--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,22 +1,27 @@
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout row wrap align-center>
-        <v-flex xs12 sm6>
+      <v-layout row wrap align-center justify-center>
+        <v-flex sm12 md6>
           <v-card>
             <v-card-title primary-title class="pb-0">
               <div class="headline">Neue Nachrichten</div>
             </v-card-title>
             <v-card-text>
               <v-list two-line>
-                <template v-for="item in items">
-                  <v-subheader v-if="item.header" :key="item.header">
-                    {{ item.header }}
-                  </v-subheader>
+                <v-list-group v-for="day in days" :key="day.header">
+                  <v-list-tile slot="activator">
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ day.header }}</v-list-tile-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                  <template v-for="msg in day.chats">
 
-                  <ChatPreview v-else :key="item.message" :item="item"></ChatPreview>
-                  <v-divider :key="item.message + 'd'"></v-divider>
-                </template>
+                    <ChatPreview :key="msg.content" :message="msg"></ChatPreview>
+                    <v-divider :key="msg.content + 'd'"></v-divider>
+                  </template>
+                </v-list-group>
+                
               </v-list>
             </v-card-text>
           </v-card>
@@ -35,9 +40,16 @@ export default {
   components: {ChatPreview},
   data: function(){
     return {
-      items: [
-        {header: 'Heute'},
-        {message: 'Heute ist wieder Freitagstreff!', meta: '<span class="text--primary">Freitagstreff</span> &mdash; Sebastian 12:13'}
+      days: [
+        {header: 'Heute',
+        chats: [
+          {avatar: require('../assets/empty.png'), content: 'Heute ist Freitagstreff!!!', meta: '<span class="text--primary">Freitagstreff</span> &mdash; Sebastian 12:13', unreadMessageCount: 1},
+          {avatar: require('../assets/empty.png'), content: 'Heute 13 Uhr!', meta: '<span class="text--primary">Alle Teamer</span> &mdash; Sebastian 12:14', unreadMessageCount: 3},
+          {avatar: require('../assets/empty.png'), content: 'Will jemand Pizza?', meta: '<span class="text--primary">Teamer HB</span> &mdash; Sebastian 12:21', unreadMessageCount: 21},
+          {avatar: require('../assets/empty.png'), content: 'Will jemand Eis?', meta: '<span class="text--primary">Teamer APG</span> &mdash; Sebastian 12:22', unreadMessageCount: 5},
+          {avatar: require('../assets/empty.png'), content: 'Ich kauf beides', meta: '<span class="text--primary">Schmitt3 RJR</span> &mdash; Sebastian 12:25', unreadMessageCount: 7},
+        ]},
+        
       ]
     }
   }
@@ -45,19 +57,9 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.v-list__group__items {
+  max-height: 50vh;
+  overflow-y: auto;
 }
 </style>
